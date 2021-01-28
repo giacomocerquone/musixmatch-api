ac49cf42799c97b49aaff97b21f3cc374b1dafa2
Giacomo Cerquone <cerquone96@hotmail.it>
2021-01-28 13:26:16 +0100

    chore: correctly extract effect function for testing

--- src/hooks/useQuizData.js
+++ src/hooks/useQuizData.js
@@ -6,74 +6,80 @@ import pickRandomElements from "utils/pickRandomElements";
 import shuffleArray from "utils/shuffleArray";
 import { showMessage } from "react-native-flash-message";
 
-export const fetchData = async (setStatus, setData) => {
-  setStatus("pending");
-  try {
-    const {
-      data: {
-        message: {
-          body: { track_list: topSongs },
+export const fetchDataEffect = async (playing, setStatus, setData) => {
+  const fetchData = async () => {
+    setStatus("pending");
+    try {
+      const {
+        data: {
+          message: {
+            body: { track_list: topSongs },
+          },
         },
-      },
-    } = await fetchTopSongs({
-      chart_name: "top",
-      page: 1,
-      page_size: 30,
-      country: "it",
-      f_has_lyrics: 1,
-    });
-    const {
-      data: {
-        message: {
-          body: { artist_list: topArtists },
+      } = await fetchTopSongs({
+        chart_name: "top",
+        page: 1,
+        page_size: 30,
+        country: "it",
+        f_has_lyrics: 1,
+      });
+      const {
+        data: {
+          message: {
+            body: { artist_list: topArtists },
+          },
         },
-      },
-    } = await fetchTopArtists({
-      page: 1,
-      page_size: 30,
-      country: "it",
-    });
-
-    const songs = pickRandomElements(topSongs, cardsPerQuiz);
-    const songsSnippets = await Promise.all(
-      songs.map((s) =>
-        fetchSongSnippet({
-          track_id: s.track.track_id,
-        })
-      )
-    );
-
-    // extract "cardsPerQuiz" songs from the top
-    // extract 2 * "cardsPerQuiz" artists from the top
-    // (2 because one must be the correct answer and therefore we use the song's artist)
-
-    const artists = pickRandomElements(topArtists, cardsPerQuiz * 2);
-
-    const data = songsSnippets.reduce((acc, s, idx) => {
-      const randomPickedArtists = artists.splice(0, cardsPerQuiz - 1);
-      const artistsToChoose = [
-        { name: songs[idx].track.artist_name, right: true },
-        ...randomPickedArtists.map((a) => ({ name: a.artist.artist_name })),
-      ];
-
-      acc.push({
-        snippet: s.data.message.body.snippet.snippet_body,
-        artists: shuffleArray(artistsToChoose),
+      } = await fetchTopArtists({
+        page: 1,
+        page_size: 30,
+        country: "it",
       });
 
-      return acc;
-    }, []);
+      const songs = pickRandomElements(topSongs, cardsPerQuiz);
+      const songsSnippets = await Promise.all(
+        songs.map((s) =>
+          fetchSongSnippet({
+            track_id: s.track.track_id,
+          })
+        )
+      );
 
-    setData(data);
-    setStatus("fulfilled");
-  } catch (e) {
-    showMessage({
-      message: "Si è verificato un errore!",
-      description:
-        "Sembra ci sia qualche problema con i nostri server, riprova più tardi!",
-      type: "danger",
-    });
-    setStatus("rejected");
+      // extract "cardsPerQuiz" songs from the top
+      // extract 2 * "cardsPerQuiz" artists from the top
+      // (2 because one must be the correct answer and therefore we use the song's artist)
+
+      const artists = pickRandomElements(topArtists, cardsPerQuiz * 2);
+
+      const data = songsSnippets.reduce((acc, s, idx) => {
+        const randomPickedArtists = artists.splice(0, cardsPerQuiz - 1);
+        const artistsToChoose = [
+          { name: songs[idx].track.artist_name, right: true },
+          ...randomPickedArtists.map((a) => ({ name: a.artist.artist_name })),
+        ];
+
+        acc.push({
+          snippet: s.data.message.body.snippet.snippet_body,
+          artists: shuffleArray(artistsToChoose),
+        });
+
+        return acc;
+      }, []);
+
+      setData(data);
+      setStatus("fulfilled");
+    } catch (e) {
+      showMessage({
+        message: "Si è verificato un errore!",
+        description:
+          "Sembra ci sia qualche problema con i nostri server, riprova più tardi!",
+        type: "danger",
+      });
+      setStatus("rejected");
+    }
+  };
+
+  if (playing) {
+    fetchData();
   }
 };
 
@@ -82,9 +88,7 @@ const useQuizData = (playing) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    if (playing) {
-      fetchData(setStatus, setData);
-    }
+    fetchDataEffect(playing, setStatus, setData);
   }, [playing]);
 
   return [status, data];
